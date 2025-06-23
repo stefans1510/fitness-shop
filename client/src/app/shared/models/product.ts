1c42eb6ec5470d18a0a1b6ecc2990d1c0d0bf0cd
Stefan <samardzicstefan6@gmail.com>
2025-06-23 13:06:58 +0200

    Initial UI setup

--- client/src/app/shared/models/product.ts
+++ client/src/app/shared/models/product.ts
@@ -3,7 +3,7 @@ export type Product = {
     name: string;
     description: string;
     price: number;
-    priceUrl: string;
+    pictureUrl: string;
     type: string;
     brand: string;
     quantityInStock: number;
---
+++ client/src/app/shared/models/shopParams.ts
@@ -0,0 +1,8 @@
+export class ShopParams {
+    types: string[] = [];
+    brands: string[] = [];
+    sort = 'name';
+    pageIndex = 1;
+    pageSize = 10;
+    search = '';
+}

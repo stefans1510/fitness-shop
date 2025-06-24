f5088897aa16df4d85f57c36a8203f18cd2647d4
Stefan <samardzicstefan6@gmail.com>
2025-06-24 19:26:38 +0200

    Angular error handling, error and loading interceptors setup

---
+++ client/src/app/features/test-error/test-error.component.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+
+@Component({
+  selector: 'app-test-error',
+  imports: [
+    MatButton
+  ],
+  templateUrl: './test-error.component.html',
+  styleUrl: './test-error.component.scss'
+})
+export class TestErrorComponent {
+  baseUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
+  validationErrors?: string[];
+
+  get404Error() {
+    this.http.get(this.baseUrl + 'bugg/notfound').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get400Error() {
+    this.http.get(this.baseUrl + 'bugg/badrequest').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get401Error() {
+    this.http.get(this.baseUrl + 'bugg/unauthorized').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get500Error() {
+    this.http.get(this.baseUrl + 'bugg/internalerror').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get400ValidationError() {
+    this.http.post(this.baseUrl + 'bugg/validationerror', {}).subscribe({
+      next: response => console.log(response),
+      error: error => this.validationErrors = error
+    })
+  }
+}
